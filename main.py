b496e0d592925a59e429bca0c188006e86e3d7e3
TimHodgson97 <timhodgson97@gmail.com>
2022-04-13 16:34:18 +0200

    notes working

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from website import create_app
+from flask_talisman import Talisman
 
 app = create_app()
 
+Talisman(app, content_security_policy=None)
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
