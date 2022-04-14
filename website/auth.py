cd3c25ee7ba932c73f94ab78e57381ddf7c7ca02
TimHodgson97 <timhodgson97@gmail.com>
2022-04-14 10:59:27 +0200

    admin authorisation

--- website/auth.py
+++ website/auth.py
@@ -90,7 +90,7 @@ def sign_up():
             db.session.commit()
             #login the new user
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Admin authorisation granted. Account created!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
